89ad57fa424272ee2755db72902ef93474aca192
Alvaro Kú <alvaro.ku.dev@gmail.com>
2025-11-06 12:04:19 -0600

    feat: api pdf generator

---
+++ PdfGeneratorApiExample/Controllers/PdfController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PdfGeneratorApiExample.Helpers;
+using PdfGeneratorApiExample.Templates;
+using PdfGeneratorApiExample.Templates.Models;
+
+namespace PdfGeneratorApiExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfController(RazorRenderer renderer, PdfGenerator pdfGenerator) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            // Generamos datos ficticios en un Modelo
+            InvoiceTemplateModel model = new InvoiceTemplateModel
+            {
+                CustomerName = "John Doe",
+                Items = [
+                                new InvoiceItemModel
+                        {
+                            Description = "Item 1",
+                            Quantity = 2,
+                            UnitPrice = 10.0,
+
+                        },
+                        new InvoiceItemModel
+                        {
+                            Description = "Item 2",
+                            Quantity = 1,
+                            UnitPrice = 35.0,
+                        },
+                        new InvoiceItemModel
+                        {
+                            Description = "Item 3",
+                            Quantity = 2,
+                            UnitPrice = 10.0,
+                        },
+                        new InvoiceItemModel
+                        {
+                            Description = "Item 4",
+                            Quantity = 3,
+                            UnitPrice = 3.0,
+                        },
+        ]
+            };
+
+            // Renderizamos el componente de Razor a un String pasando los datos del Modelo
+            var html = await renderer.RenderComponentAsync<InvoiceTemplate>(new Dictionary<string, object?>
+            {
+                { "Model", model }
+            });
+
+            // Ese mismo String, que contiene todo el HTML, lo convertimos en el documento PDF
+            var pdfBytes = await pdfGenerator.GeneratePdfAsync(html);
+
+            // Guardamos el archivo PDF de forma local
+            System.IO.File.WriteAllBytes("invoice.pdf", pdfBytes);
+
+            // La api descarga el PDF en el navegador
+            return File(pdfBytes, "application/pdf", "invoice.pdf");
+        }
+    }
+}
---
+++ PdfGeneratorApiExample/Templates/Models/InvoiceModels.cs
@@ -0,0 +1,17 @@
+﻿namespace PdfGeneratorApiExample.Templates.Models
+{
+    public class InvoiceTemplateModel
+    {
+        public string CustomerName { get; set; }
+        public IEnumerable<InvoiceItemModel> Items { get; set; }
+
+    }
+
+    public class InvoiceItemModel
+    {
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+        public double Total => Quantity * UnitPrice;
+    }
+}
